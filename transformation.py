d3f3a5cc671e09e54c5b28eb1491dcc6048f9b93
Fitz Hofer <fitzhofer@gmail.com>
2026-08-19 19:07:15 +0800

    Formed 2 neurons fundamentally

---
+++ transformation.py
@@ -0,0 +1,56 @@
+
+A_1 = [
+    [1, 2, -1, 3],
+    [2, 1, 4, 0],
+    [-1, 3, 2, 1]
+]
+
+v = [2, 1, -1, 3]
+b_1 = [1, -2, 3]
+
+A_2 = [
+    [1, -2, 3],
+    [2, 1, -1]
+]
+
+b_2 = [-1, 4]
+
+def MatrixVectorMul(A, x):
+    if len(A[0]) != len(x):
+        raise ValueError("Number of columns in a neuron must match the number of rows from the input")
+    Ax = []
+    for i in range(len(A)):
+        row_sum = 0
+        for j in range(len(A[i])):
+            row_sum += x[j] * A[i][j]
+        Ax.append(row_sum)
+    return Ax
+
+def dot_product(a, b):
+    if len(a) != len(b):
+        raise ValueError("Vectors must be the same length")
+    sum = 0
+    for i in range(len(a)):
+        sum += a[i] * b[i]
+    return sum
+
+def activation(x, W, b):
+    pre_act = dot_product(W, x) + b
+    if pre_act > 0:
+        return pre_act
+    else:
+        return 0
+
+def layers(x, W, b):
+    z = MatrixVectorMul(W, x)
+    for i in range(len(z)):
+        z[i] = z[i] + b[i]
+        if z[i] > 0:
+            z[i] = z[i]
+        else:
+            z[i] = 0
+    return z
+
+layer = layers(v, A, b)
+print(layer)
+
