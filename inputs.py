98ed39572bd67dbe4e5c2d7fb6923457c8a39eba
Fitz Hofer <fitzhofer@gmail.com>
2026-08-21 17:24:06 +0800

    Testing neurons with inputs

---
+++ inputs.py
@@ -0,0 +1,50 @@
+from transformation import layers, MatrixVectorMul
+
+p = [
+    [3, 2],
+    [1, 3],
+    [3, 1],
+    [1, 1]
+]
+
+W = [
+    [1, 1],
+    [-1, 1],
+    [1, -1]
+]
+
+b = [-2, -1, -1]
+
+a_1 = []
+
+W_2 = [
+    [1, 2, -1],
+    [-2, 1, 1]
+]
+
+b_2 = [-1, 0]
+
+a_2 = []
+
+print("Activated Outputs")
+for i in range(len(p)):
+    x = p[i]
+    a = layers(x, W, b)
+    print(f"Input: {x}, Output: {a}")
+    a_1.append(a)
+
+print("Pre-Activation Outputs")
+for i in range(len(p)):
+    z = MatrixVectorMul(W, p[i])
+    for j in range(len(z)):
+        z[j] = z[j] + b[j]
+    print(f"Input: {p[i]}, Output: {z}")
+
+print("Layer 2")
+for i in range(len(a_1)):
+    a = layers(a_1[i], W_2, b_2)
+    a_2.append(a)
+
+for i in a_2:
+    print(f"Activation Output: {i}")
+
