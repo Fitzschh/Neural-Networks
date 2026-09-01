087ec7bc8af8687ea84d0d2d21d09614247e7ca5
Fitz Hofer <fitzhofer@gmail.com>
2026-09-01 12:07:30 +0800

    Forming initial architecture

---
+++ training.py
@@ -0,0 +1,8 @@
+from functions import hidden_layers, softmax, logits, cross_entropy_loss, loss_gradients, backpropagation, dReLU, vector_mul, gradient_descent, gradient_descent_bias, gradient
+from inputs import M, b, seven, M2, b2, OL, b3, t, n
+
+
+def train(M, b, seven, M2, b2, OL, b3, t, n):
+
+    for epoch in range():
+        pass
