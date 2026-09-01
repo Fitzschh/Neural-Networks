9b6e021676fcbcd96e9ce60c1df09c6948a1a34b
Fitz Hofer <fitzhofer@gmail.com>
2026-09-01 12:59:25 +0800

    Finished forward propagation loop

--- training.py
+++ training.py
@@ -2,7 +2,20 @@ from functions import hidden_layers, softmax, logits, cross_entropy_loss, loss_g
 from inputs import M, b, seven, M2, b2, OL, b3, t, n
 
 
-def train(M, b, seven, M2, b2, OL, b3, t, n):
+def train(M, b, seven, M2, b2, OL, b3, t, n, iter):
 
-    for epoch in range():
-        pass
+    for epoch in range(iter):
+        #Forward propagation
+        a = hidden_layers(M, seven, b) #First Layer
+
+        a2 = hidden_layers(M2, a, b2) #Second Layer
+
+        z = logits(OL, a2, b3) #Output Layer
+
+        y = softmax(z) 
+
+        #Backpropagation
+        dLdzi = loss_gradients(y, t)
+
+
+        
