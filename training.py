3d1e08246f8d90dd23a0cb9ac34d891cdbb12909
Fitz Hofer <fitzhofer@gmail.com>
2026-09-02 12:38:40 +0800

    Backpropagation up to Layer 1

--- training.py
+++ training.py
@@ -15,7 +15,29 @@ def train(M, b, seven, M2, b2, OL, b3, t, n, iter):
         y = softmax(z) 
 
         #Backpropagation
-        dLdzi = loss_gradients(y, t)
+        dLdzi = loss_gradients(y, t)#Loss with respect to logits
+
+        dLda2 = backpropagation(OL, dLdzi)#Loss with respect to activated values from Layer 2
+
+        da2dz2 = dReLU(a2)#Derivative of ReLU in Layer 2
+
+        dLdz2 = vector_mul(dLda2, da2dz2)#Backpropagation of Layer 2
+
+        dLda = backpropagation(M2, dLdz2)#Loss with respect to activated values from Layer 1
+
+        dadz = dReLU(a)#Derivative of ReLU in Layer 1
+
+        dLdz = vector_mul(dLda, dadz)#Backpropagation of Layer 1
+
+        dLdW = gradient(dLdz, seven)#Distribute values of backpropagated outputs to each weights in Layer 1
+
+        
+
+
+
+
+
+
 
 
         
