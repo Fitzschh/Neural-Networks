5bf5463ebca68f36912547aef71c114136b39cd5
Fitz Hofer <fitzhofer@gmail.com>
2026-09-05 19:07:35 +0800

    More testing

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from functions import image_to_vector
 from training import train
 from inputs import M, b, M2, b2, OL, b3, t, n, t2
 
-x = image_to_vector("images/seven_test1.png")
+x = image_to_vector("images/five_test1.png")
 
-train(M, b, x, M2, b2, OL, b3, t, t2, n, 10)
+train(M, b, x, M2, b2, OL, b3, t, t2, n, 4)
