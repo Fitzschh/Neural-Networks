860acf31ba1b722a34883e23659621b211c980de
Fitz Hofer <fitzhofer@gmail.com>
2026-09-06 14:42:50 +0800

    First MNSIT image processed successfully

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from functions import image_to_vector
 from training import train
 from inputs import M, b, M2, b2, OL, b3, t, n, t2
 
-x = image_to_vector("images/five_test1.png")
+file = open("images/t10k-images-idx3-ubyte", "rb")
+data = file.read(16)
+print(list(data))
 
-train(M, b, x, M2, b2, OL, b3, t, t2, n, 4)
+image = file.read(784)
+print(list(image))
+
+pixels = list(image)
+
+
+inputs = []
+
+for pixel in pixels:
+    inputs.append(pixel / 255)
+
+print(inputs)
+
+train(M, b, inputs, M2, b2, OL, b3, t, t2, n, 4)
