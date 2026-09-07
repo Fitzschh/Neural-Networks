c60594d3f756f050040302aff6f1d480b70b44bc
Fitz Hofer <fitzhofer@gmail.com>
2026-09-07 14:26:17 +0800

    Shifted to a Stochastic Gradient Approach to avoid memorization, and implement generalization; compared to the previous experiment, it was able to move from 2/10 correct predictions(20% accuracy) into 3/10 (30% accuracy). Images were also increased from 10 to 100

--- main.py
+++ main.py
@@ -8,22 +8,31 @@ data = file.read(16)
 file2 = open("images/t10k-labels-idx1-ubyte", "rb")
 data = file2.read(8)
 
-#Training
-for i in range(10):
+
+images = []
+labels = []
+#Read images and labels
+for i in range(100):
     image = file.read(784)
     label = file2.read(1)
 
-    label = label[0]
-    target = label_to_vector(label)
-    print(target)
-    pixels = list(image)
+    images.append(image)
+    labels.append(label[0])
 
-    inputs = []
+#Training
+for epoch in range(10):
 
-    for pixel in pixels:
-        inputs.append(pixel / 255)
+    for i in range(len(images)):
 
-    M, b, M2, b2, OL, b3 = train(M, b, inputs, M2, b2, OL, b3, target, label, n, 20)
+        target = label_to_vector(labels[i])
+        print(target)
+        pixels = list(images[i])
+        inputs = []
+                
+        for pixel in pixels:
+            inputs.append(pixel / 255)
+
+        M, b, M2, b2, OL, b3 = train(M, b, inputs, M2, b2, OL, b3, target, labels[i], n, 1)
 
 #Prediction Only
 for i in range(10):
