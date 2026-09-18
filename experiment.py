92d497c66087e2bedcf797d2573c1d228409b4b1
Fitz Hofer <fitzhofer@gmail.com>
2026-09-18 15:45:44 +0800

    Due to Stochastic Gradient Descent having approached its current limits, the algorithm has been switched to Batch Gradient Training.

--- experiment.py
+++ experiment.py
@@ -1,5 +1,5 @@
 from functions import image_to_vector, label_to_vector
-from training import train, predict
+from training import train, predict, batch_gradient, batch_descent
 from inputs import M, b, M2, b2, OL, b3, n, t2
 import matplotlib.pyplot as plt
 import time
@@ -26,8 +26,8 @@ avg_loss = []
 accuracy_list = []
 training_times = []
 
-training_images = 60000
-
+training_images = 1000
+batch_size = 32
 
 for j in range(training_images):
     image = file.read(784)
@@ -36,11 +36,12 @@ for j in range(training_images):
     images.append(image)
     labels.append(label[0])
 
-    training_start = time.perf_counter()
 
 n = 0.01
 
-for epoch in range(70):
+training_start = time.perf_counter()
+
+for epoch in range(10):
 
     total_loss = 0
 
@@ -49,18 +50,103 @@ for epoch in range(70):
 
     random.shuffle(indices)
 
-    for i in indices:
+    for start in range(0, len(images), batch_size):
 
-        target = label_to_vector(labels[i])
-        #print(target)
-        pixels = list(images[i])
-        inputs = []
+        batch_indices = indices[start:start + batch_size]
+
+        W_gradient_list = []
+        b_gradient_list = []
+        W2_gradient_list = []
+        b2_gradient_list = []
+        OL_gradient_list = []
+        b3_gradient_list = []
+
+        for i in batch_indices:
+
+            target = label_to_vector(labels[i])
+            #print(target)
+            pixels = list(images[i])
+            inputs = []
                     
-        for pixel in pixels:
-            inputs.append(pixel / 255)
+            for pixel in pixels:
+                inputs.append(pixel / 255)
 
-        M, b, M2, b2, OL, b3, loss = train(M, b, inputs, M2, b2, OL, b3, target, labels[i], n, 1)
-        total_loss += loss
+            dLdW, dLdb, dLdW2, dLdb2, dLdOL, dLdb3, loss = batch_gradient(M, b, M2, b2, OL, b3, inputs, labels[i], target, 1)
+            total_loss += loss
+            W_gradient_list.append(dLdW)
+            b_gradient_list.append(dLdb)
+            W2_gradient_list.append(dLdW2)
+            b2_gradient_list.append(dLdb2)
+            OL_gradient_list.append(dLdOL)
+            b3_gradient_list.append(dLdb3)
+
+        #Averaging the gradients of W
+        avg_gradient_W = []
+
+        for row in range(128):
+
+            avg_row = []
+
+            for col in range(784):
+
+                gradient_sum = 0
+
+                for g in W_gradient_list:
+                    gradient_sum += g[row][col]
+
+                average = gradient_sum / len(W_gradient_list)
+
+                avg_row.append(average)
+
+            avg_gradient_W.append(avg_row)
+
+        #Averaging the gradients of b
+        avg_gradient_b = [sum(g[i] for g in b_gradient_list) / len(b_gradient_list) for i in range(128)]
+
+        avg_gradient_W2 = []
+
+        for row in range(64):
+
+            avg_row = []
+
+            for col in range(128):
+
+                gradient_sum = 0
+
+                for g in W2_gradient_list:
+                    gradient_sum += g[row][col]
+
+                average = gradient_sum / len(W2_gradient_list)
+
+                avg_row.append(average)
+
+            avg_gradient_W2.append(avg_row)
+
+        avg_gradient_b2 = [sum(g[i] for g in b2_gradient_list) / len(b2_gradient_list) for i in range(64)]
+
+        avg_gradient_OL = []
+
+        for row in range(10):
+            
+            avg_row = []
+
+            for col in range(64):
+
+                gradient_sum = 0
+
+                for g in OL_gradient_list:
+                    gradient_sum += g[row][col]
+
+                average = gradient_sum / len(OL_gradient_list)
+
+                avg_row.append(average)
+
+            avg_gradient_OL.append(avg_row)
+
+        avg_gradient_b3 = [sum(g[i] for g in b3_gradient_list) / len(b3_gradient_list) for i in range(10)]
+
+        #Updating weights and biases
+        M, b, M2, b2, OL, b3 = batch_descent(M, b, M2, b2, OL, b3, avg_gradient_W, avg_gradient_b, avg_gradient_W2, avg_gradient_b2, avg_gradient_OL, avg_gradient_b3, n)
 
     n = n * 0.95
 
@@ -77,8 +163,7 @@ print(f"Training time for {training_images} images: {training_time:.2f} seconds"
 
 correct_pred = []
 #Prediction Only
-num_of_images = 1000
-
+num_of_images = 10000
 
 file3.seek(16)
 file4.seek(8)
--- training.py
+++ training.py
@@ -83,8 +83,69 @@ def predict(M, b, input, M2, b2, OL, b3):
 def save_model():
     pass
 
-def batch_train():
-    pass
+def batch_gradient(M, b, M2, b2, OL, b3, input, target, t, iter):
+
+    for epoch in range(iter):
+        #Forward propagation
+        a = hidden_layers(M, input, b) #First Layer
+    
+        a2 = hidden_layers(M2, a, b2) #Second Layer
+    
+        z = logits(OL, a2, b3) #Output Layer
+    
+        y = softmax(z) 
+    
+        loss = cross_entropy_loss(y, target)
+    
+        #Backpropagation
+        dLdzi = loss_gradients(y, t)#Loss with respect to logits
+    
+        dLda2 = backpropagation(OL, dLdzi)#Loss with respect to activated values from Layer 2
+    
+        da2dz2 = dReLU(a2)#Derivative of ReLU in Layer 2
+    
+        dLdz2 = vector_mul(dLda2, da2dz2)#Backpropagation of Layer 2
+    
+        dLda = backpropagation(M2, dLdz2)#Loss with respect to activated values from Layer 1
+    
+        dadz = dReLU(a)#Derivative of ReLU in Layer 1
+    
+        dLdz = vector_mul(dLda, dadz)#Backpropagation of Layer 1
+    
+        dLdW = gradient(dLdz, input)#Distribute values of backpropagated outputs to each weights in Layer 1
+
+        dLdb = dLdz
+
+        dLdW2 = gradient(dLdz2, a)#Distribute values of backpropagated outputs to each weights in Layer 2
+
+        dLdb2 = dLdz2
+
+        dLdOL = gradient(dLdzi, a2)#Distribute values of backpropagated outputs to each weights in Output Layer
+
+        dLdb3 = dLdzi
+
+    return dLdW, dLdb, dLdW2, dLdb2, dLdOL, dLdb3, loss
+
+def batch_descent(M, b, M2, b2, OL, b3, dLdW, dLdb, dLdW2, dLdb2, dLdOL, dLdb3, n):
+    M = gradient_descent(M, dLdW, n)#Weights of 1st Layer altered
+
+    b = gradient_descent_bias(b, dLdb, n)#Biases of 1st Layer altered
+
+    M2 = gradient_descent(M2, dLdW2, n)#Weights of 2nd Layer altered
+
+    b2 = gradient_descent_bias(b2, dLdb2, n)#Biases of 2nd Layer altered
+
+    OL = gradient_descent(OL, dLdOL, n)#Weights of Output Layer altered
+
+    b3 = gradient_descent_bias(b3, dLdb3, n)#Biases of Output Layer altered
+
+    return M, b, M2, b2, OL, b3
+
+    
+
+    
+
+    
 
 
 
